3d15c2f91543fa026a8a03c7cfbac6114ea0b29b
Wilson.egas <Wilson.egas@udla.edu.ec>
2023-11-01 15:57:54 -0500

    llamamos a la funcion para poder realizar el calculo necesario

--- ejercicio.c
+++ ejercicio.c
@@ -6,16 +6,16 @@ int division (int a1, int b1 ){
 int main(){
     int numero1=0;
     int numero2=2;
-    int numero3=-1;
+    int numero3=1;
 
-    int resultado;
+    float resultado;
 
     printf("Escriba el dato de entrada: ");
-    scanf("%d",&numero1);
+    scanf("%f",&numero1);
 
-    resultado=numero1/numero2/numero3;
+    resultado=numero1/numero2+numero3;
 
-    printf("El valor de salida es: %d ",resultado);
+    printf("El valor de salida es: %f ",resultado);
 
  return 0;
 }
