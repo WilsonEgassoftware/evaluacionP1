ab0785d2d07fa1e5ee1fc40f0eadba1117f87f27
Wilson.egas <Wilson.egas@udla.edu.ec>
2023-11-01 16:09:01 -0500

    Realizamos el procedimiento de prueba para finalmente obtener el resultado

--- ejercicio.c
+++ ejercicio.c
@@ -4,16 +4,15 @@ int division (int a1, int b1 ){
     int resultado=a1/b1;
 }
 int main(){
+    int numero3=1;
     int numero1=0;
     int numero2=2;
-    int numero3=1;
-
     float resultado;
 
     printf("Escriba el dato de entrada: ");
-    scanf("%f",&numero1);
+    scanf("%d",&numero1);
 
-    resultado=numero1/numero2+numero3;
+    resultado=numero1%numero2;
 
     printf("El valor de salida es: %f ",resultado);
 
