8032e22be70bbaf214a3209717295000db96e5c8
Wilson.egas <Wilson.egas@udla.edu.ec>
2023-11-01 15:36:48 -0500

    Inicializamos la varibles que usaremos en los datos de entrada

---
+++ ejercicio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int division (int a1, int b1, int c1){
+    int resultado=a1/b1+c1;
+}
+int main(){
+    int numero1;
+    int numero2;
+
+    printf("Escriba el dato de entrada");
+    scanf("%d",&numero1);
+
+    
+ return 0;
+}
