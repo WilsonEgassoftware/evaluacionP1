3e4474c1b1fa9685f162968b9a7498c0eab6cdbe
Wilson.egas <Wilson.egas@udla.edu.ec>
2023-11-01 15:47:38 -0500

    Realizamos la funcion diviosn que nos ayudara a poder obtener los valores que se piden

--- ejercicio.c
+++ ejercicio.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
-int division (int a1, int b1, int c1){
-    int resultado=a1/b1+c1;
+int division (int a1, int b1 ){
+    int resultado=a1/b1;
 }
 int main(){
-    int numero1;
-    int numero2;
+    int numero1=0;
+    int numero2=5;
 
-    printf("Escriba el dato de entrada");
+    int resultado;
+
+    printf("Escriba el dato de entrada: ");
     scanf("%d",&numero1);
 
-    
+    resultado=numero1/numero2;
+
+    printf("El valor de salida es: %d ",resultado);
+
  return 0;
 }
