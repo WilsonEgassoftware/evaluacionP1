769e58e4ee13c0b7dfc500a39966283e1f0658d4
Wilson.egas <Wilson.egas@udla.edu.ec>
2023-11-01 15:52:09 -0500

    Pedimos el ingreso de los datos  para evaludarlos y guardar el dato

--- ejercicio.c
+++ ejercicio.c
@@ -5,14 +5,15 @@ int division (int a1, int b1 ){
 }
 int main(){
     int numero1=0;
-    int numero2=5;
+    int numero2=2;
+    int numero3=-1;
 
     int resultado;
 
     printf("Escriba el dato de entrada: ");
     scanf("%d",&numero1);
 
-    resultado=numero1/numero2;
+    resultado=numero1/numero2/numero3;
 
     printf("El valor de salida es: %d ",resultado);
 
