f3183b20a8e357fd03c2fd36532467ccf4ba2d18
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2023-11-01 16:16:12 -0500

    Update ejercicio.c

--- ejercicio.c
+++ ejercicio.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int division (int a1, int b1 ){
+int division (int a1, int b1 ){//declaramos la funcion que vamos a usar en el ejercicio
     int resultado=a1/b1;
 }
 int main(){
-    int numero3=1;
+    int numero3=1;//incializamos los datos enteros y decimales
     int numero1=0;
     int numero2=2;
-    float resultado;
+    float resultado;//declaramos este valor en flotante para obtener el valor decimal
 
-    printf("Escriba el dato de entrada: ");
+    printf("Escriba el dato de entrada: ");//pedmos el ingreso del numero para poder saber que numero vamos a dividir
     scanf("%d",&numero1);
 
-    resultado=numero1%numero2;
+    resultado=numero1%numero2;//llamamos a la funcion para obtener el resultado de la division
 
-    printf("El valor de salida es: %f ",resultado);
+    printf("El valor de salida es: %f ",resultado);//finalmente debemos obtener el valor de la division
 
  return 0;
 }
